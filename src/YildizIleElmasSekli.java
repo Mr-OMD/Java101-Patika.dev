8df588cc527fadc5cef9f0d83b0885022d9a5a1e
Ömer Mert Demirel <demirel.omert@gmail.com>
2021-11-14 13:46:38 +0300

    Harmonik Seri eklendi

---
+++ src/YildizIleElmasSekli.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class YildizIleElmasSekli {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Sayı girin: ");
+        int sayi = in.nextInt();
+
+        for (int i = 0; i < sayi; i++) {
+            for (int j = 0; j < (sayi - i); j++)
+                System.out.print(" ");
+            for (int k = 1; k <= (2 * i + 1); k++)
+                System.out.print("*");
+            System.out.println("");
+        }
+        for (int i = sayi; i >= 0; i--) {
+            for (int j = 0; j < (sayi - i); j++)
+                System.out.print(" ");
+            for (int k = 1; k <= (2 * i + 1); k++)
+                System.out.print("*");
+            System.out.println("");
+        }
+    }
+}
