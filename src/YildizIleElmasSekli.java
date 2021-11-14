93e8c30bb5729c60848ae759fde0f5e7703e834a
Ömer Mert Demirel <demirel.omert@gmail.com>
2021-11-14 13:47:40 +0300

    Yildiz ile elmaz şekli eklendi

--- src/YildizIleElmasSekli.java
+++ src/YildizIleElmasSekli.java
@@ -11,14 +11,14 @@ public class YildizIleElmasSekli {
                 System.out.print(" ");
             for (int k = 1; k <= (2 * i + 1); k++)
                 System.out.print("*");
-            System.out.println("");
+            System.out.println(" ");
         }
         for (int i = sayi; i >= 0; i--) {
             for (int j = 0; j < (sayi - i); j++)
                 System.out.print(" ");
             for (int k = 1; k <= (2 * i + 1); k++)
                 System.out.print("*");
-            System.out.println("");
+            System.out.println(" ");
         }
     }
 }
